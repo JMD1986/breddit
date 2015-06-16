0883890856ea2d61e8a198028afd5eb388482410
John Daugherty <johndaugherty1986@gmail.com>
2015-06-16 15:02:17 -0500

    heroku push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
   def current_user
     if @current_user.nil?
       if session[:user_id].present?
